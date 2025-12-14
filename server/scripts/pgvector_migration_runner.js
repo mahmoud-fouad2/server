30db45bd13dd217dd0fbcbba2bb9d395f3ce5a53
Mahmoud-Fouad2 <mahmoud.a.fouad2@gmail.com>
2025-12-14 20:41:15 +0300

    chore(cleanup): remove placeholder migration runner

--- server/scripts/pgvector_migration_runner.js
+++
@@ -1,6 +0,0 @@
-// Removed: migration runner - was used temporarily to migrate embeddings to pgvector
-// Migration completed and runner removed.
-
-// (This file left intentionally blank to preserve history. It will be deleted in a follow-up commit.)
-
-module.exports = {};
