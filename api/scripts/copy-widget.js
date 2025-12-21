dc0c18ef4d167776729c377eccfad886258a34fe
Mahmoud-Fouad2 <mahmoud.a.fouad2@gmail.com>
2025-12-21 10:51:25 +0300

    Fix: Convert copy-widget script to ES modules to match project type

--- api/scripts/copy-widget.js
+++ api/scripts/copy-widget.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const source = path.join(__dirname, '../../widget/dist/fahimo-loader.iife.js');
 const destDir = path.join(__dirname, '../public');
